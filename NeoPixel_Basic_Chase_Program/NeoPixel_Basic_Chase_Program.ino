6539a341a992471be6a891772f6510d925a15d5b
avrawat <rabhinav452@gmail.com>
2021-11-08 22:05:53 +0530

    first commit of the codes2

--- NeoPixel_Basic_Chase_Program/NeoPixel_Basic_Chase_Program.ino
+++ NeoPixel_Basic_Chase_Program/NeoPixel_Basic_Chase_Program.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 //
-//This is a simple LED chase program for the WS2811/2 RGB led strip. 
+//This is a simple LED chase program for the WS2811/2 RGB led strip.
 //Connect the data wire to pin 8 of arduino
 //
 
@@ -20,18 +20,18 @@ int delayNum;
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  
+
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS); // GRB ordering is assumed
   // intialise the index
   i = 0;
   // size of the moving window - 9 is the number of green leds
-  window = 3;
-  delayNum = 1000;
-  
+  window = 1;
+  delayNum = 500;
+
 }
 
 void loop() {
- 
+
   newChase();
 
 }
@@ -43,45 +43,26 @@ void newChase() {
   for (i = 0; i <= NUM_LEDS + window; i++) {
 
     // untill we are in the limit
-    
+
     if (i < NUM_LEDS) {
       // first led to be red
       leds[i] = CRGB::Red;
-
+    }
     // turn the #window leds to green behind the red one
-        j=i;
-        while (--j >= (i - window)) {
-          if(j<0){
-            break;
-          }else{
-            leds[j] = CRGB::Green;
-          }
-        }
-
-        // close behind windows
-        int k = window + 1;
-        leds[i - k] = CRGB::Black;
-
-    } else {
-
-      // keep the animation moving with the green leds
-      // don't turn on red leds
-      // leds[i] = CRGB::Red;
-    
-      j = i;
-      while (--j >= (i - window)) {
-        if (j < NUM_LEDS) {
-          leds[j] = CRGB::Green;
-        }
+    j = i;
+    while (--j >= (i - window)) {
+      if (j < 0 || j >= NUM_LEDS) {
+        break;
+      } else {
+        leds[j] = CRGB::Green;
       }
+    }
 
-      // close behind windows
-      int k = window + 1;
-      if (i - k < NUM_LEDS) {
-        leds[i - k] = CRGB::Black;
-      }
-
-    }//else
+    // close behind windows
+    int cls = i - (window + 1);
+    if( cls >= 0 && cls < NUM_LEDS){
+         leds[cls] = CRGB::Black;
+    }
 
     FastLED.show();
     delay(delayNum);
